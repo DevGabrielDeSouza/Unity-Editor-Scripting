bb693daab83e571e50c2ccd33c40d5cd68f22916
Gabriel de Souza <devgabrieldesouza@gmail.com>
2018-11-04 03:51:22 -0200

    Spawning a Window
    Basic window creation script.

--- Assets/Editor/EnemyDesignerWindow.cs
+++ Assets/Editor/EnemyDesignerWindow.cs
@@ -8,9 +8,11 @@ public class EnemyDesignerWindow : EditorWindow {
 
 	[MenuItem("Window/Enemy Designer")]
 	static void OpenWindow(){
+
 		EnemyDesignerWindow window = (EnemyDesignerWindow)GetWindow(typeof (EnemyDesignerWindow));
 		window.minSize = new Vector2(600, 300);
 		window.Show();
+		
 	}
 }
 
